2d7b13c8706baec0482178123de2988e0e277e02
brianbartilet <brian.bartilet@gmail.com>
2024-05-07 21:39:44 +0800

    (work) update tests aaa

--- work/apps/.template/config.py
+++ work/apps/.template/config.py
@@ -2,8 +2,24 @@ import os
 from work.apps.apps_config import CONFIG_MANAGER
 from core.web.browser.core.config.web_driver import AppConfigWebDriver
 
+# Get the application name from the directory of the current file
 APP_NAME = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
-CONFIG_MANAGER.load(APP_NAME)
 
-# Initialize the application configuration for a web driver using settings from the loaded configuration.
-CONFIG = CONFIG_MANAGER.get(AppConfigWebDriver, 'config_name')
+
+def load(config_id: str):
+    """
+    Load and retrieve the web driver configuration for the specified application.
+
+    This function first uses the global CONFIG_MANAGER to load configurations specific
+    to the application determined by the APP_NAME. It then retrieves the web driver
+    configuration using the provided configuration identifier.
+
+    Args:
+        config_id: The identifier for the specific configuration to be retrieved.
+
+    Returns:
+        AppConfigWebDriver: An instance of the AppConfigWebDriver containing the
+                            configuration details for the web driver.
+    """
+    CONFIG_MANAGER.load(APP_NAME)
+    return CONFIG_MANAGER.get(AppConfigWebDriver, config_id)
---
+++ work/apps/aaa/references/pages/__init__.py
@@ -0,0 +1,6 @@
+from .account import PageAAATradingDeskAccount
+from .login import PageAAALogin
+from .market import PageAAATradingDeskMarket
+from .orders import PageAAATradingDeskOrders
+from .portfolio import PageAAATradingDeskPortfolio
+
