5d13b72b69b45de27381d9ba9ab012f064c5ed91
brianbartilet <brian.bartilet@gmail.com>
2025-10-08 13:32:47 +0800

    (chore) add tasks demo scheduler/worker

---
+++ demo/builder/__init__.py
@@ -0,0 +1,2 @@
+from demo.builder.workflows.do_math import *
+from demo.builder.workflows.send_web_requests import *
---
+++ demo/builder/workflows/send_web_requests.py
@@ -0,0 +1,7 @@
+from core.apps.sprout.app.celery import SPROUT
+from uuid import uuid4
+
+
+@SPROUT.task()
+def send_requests():
+    return uuid4()
