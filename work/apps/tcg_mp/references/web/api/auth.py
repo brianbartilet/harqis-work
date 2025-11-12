0f0abd4e3d9f82b2164df9e81889e46557d319cf
brianbartilet <brian.bartilet@gmail.com>
2025-11-12 21:23:18 +0800

    (work) add auth to tcgmp

--- work/apps/tcg_mp/references/web/api/auth.py
+++ work/apps/tcg_mp/references/web/api/auth.py
@@ -1,32 +1,33 @@
-from work.apps.echo_mtg.references.web.base_api_service import BaseApiServiceAppEchoMtg
+from work.apps.tcg_mp.references.web.base_api_service import BaseApiServiceAppTcgMp
 from core.web.services.core.decorators.deserializer import deserialized
 
 
-class ApiServiceEchoMTGAuth(BaseApiServiceAppEchoMtg):
+class ApiServiceTcgMpAuth(BaseApiServiceAppTcgMp):
 
     def __init__(self, config, **kwargs):
-        super(ApiServiceEchoMTGAuth, self).__init__(config, **kwargs)
+        super(ApiServiceTcgMpAuth, self).__init__(config, **kwargs)
         self.username = config.app_data['username']
         self.password = config.app_data['password']
         self.token = None
-
+        self.user_id = None
         self.initialize()
 
     def initialize(self):
         self.request\
-            .set_base_uri('user')
+            .set_base_uri('auth')
 
     @deserialized(dict)
     def authenticate(self, username: str = None, password: str = None):
         self.request.add_uri_parameter('auth')
         payload = {
-            'email': username if username is not None else self.username,
-            'password': password if password is not None else self.password
+            'user': username if username is not None else self.username,
+            'pwd': password if password is not None else self.password
         }
         self.request.post() \
             .add_json_payload(payload)
 
         response = self.client.execute_request(self.request.build())
-        self.token = response.data['token']
-        return response
+        self.token = response.data['accessToken']
+        self.user_id = response.data['id']
 
+        return response
