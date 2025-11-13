a7a9e77ad7bb0e018238239c88a1c57004925c96
brianbartilet <brian.bartilet@gmail.com>
2025-11-14 01:32:05 +0800

    (work) TCGMP: add more endpoints for orders

--- apps/tcg_mp/references/dto/order.py
+++ apps/tcg_mp/references/dto/order.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, List
+from enum import Enum
 
 
 @dataclass
@@ -18,3 +19,15 @@ class DtoOrderSummary:
     is_transfer: Optional[int] = None
     is_buy_voucher: Optional[int] = None
     crd_foil_type: Optional[str] = None
+
+@dataclass
+class DtoOrderSummaryByStatus:
+    """Represents a collection of order summaries filtered by status."""
+    data: Optional[List[DtoOrderSummary]] = None
+    status: Optional[int] = None
+
+class EnumTcgOrderStatus(Enum):
+    PENDING_DROP_OFF = (1, "Pending Drop Off")
+    ARRIVED_BRANCH = (4, "Arrived at Branch")
+    DROPPED = (6, "Dropped Off")
+    CANCELLED = (7, "Cancelled")
