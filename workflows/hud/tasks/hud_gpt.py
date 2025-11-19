a90f9f49375048162bf3d8713e41572f3d1e69b0
brianbartilet <brian.bartilet@gmail.com>
2025-11-19 12:01:53 +0800

    (work) HUD: added gpt desk checker

--- workflows/hud/tasks/hud_gpt.py
+++ workflows/hud/tasks/hud_gpt.py
@@ -99,7 +99,7 @@ def get_helper_information(ini=ConfigHelperRainmeter()):
     ini['meterTitle']['W'] = '({0}*190*#Scale#)'.format(width_multiplier)
     ini['meterTitle']['X'] = '({0}*190*#Scale#)/2'.format(width_multiplier)
 
-    dump = wrap_text(answer_, width=72)
+    dump = wrap_text(answer_, width=65)
 
     ini['Variables']['ItemLines'] = '{0}'.format(2 + len(re.findall(r'\r\n|\r|\n', dump)))
 
