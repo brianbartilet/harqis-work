12d41ab49c59d7db9663ea64322f62bd65272906
brianbartilet <brian.bartilet@gmail.com>
2025-11-22 17:58:28 +0800

    (work) HUD: error logs

--- workflows/hud/__init__.py
+++ workflows/hud/__init__.py
@@ -3,3 +3,4 @@ import workflows.hud.tasks.hud_forex
 import workflows.hud.tasks.hud_tcg
 import workflows.hud.tasks.hud_gpt
 import workflows.hud.tasks.hud_calendar
+import workflows.hud.tasks.hud_logs
