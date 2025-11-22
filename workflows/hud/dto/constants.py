761a75d0a43120d0a640c9d01a6135031b5804f2
brianbartilet <brian.bartilet@gmail.com>
2025-11-22 13:50:04 +0800

    (work) HUD: smart hud from schedule

--- workflows/hud/dto/constants.py
+++ workflows/hud/dto/constants.py
@@ -5,6 +5,8 @@ class ScheduleCategory(str, Enum):
     PINNED = "Pinned"
     PLAY = "Mischief | Misdirection | Play"
     FINANCE = "Finance | Investing | Business"
+    WORK = "Career | Work"
+    ORGANIZE = "Organization | Everyman Skills"
 
     def __str__(self) -> str:
         return self.value
--- workflows/hud/tasks/hud_gpt.py
+++ workflows/hud/tasks/hud_gpt.py
@@ -12,6 +12,9 @@ from apps.rainmeter.config import CONFIG as RAINMETER_CONFIG
 from core.apps.gpt.assistants.base import BaseAssistant
 from core.apps.gpt.models.assistants.message import MessageCreate
 from core.apps.gpt.models.assistants.run import RunCreate
+from core.utilities.logging.custom_logger import logger as log
+
+from workflows.hud.dto.constants import ScheduleCategory
 
 _sections__check_desktop = {
     "meterLink_github": {
@@ -24,12 +27,13 @@ _sections__check_desktop = {
 @init_meter(RAINMETER_CONFIG,
             hud_item_name='GPT DESK CHECK',
             new_sections_dict=_sections__check_desktop,
-            play_sound=False)
-def get_helper_information(ini=ConfigHelperRainmeter()):
-
+            play_sound=False,
+            schedule_categories=[ScheduleCategory.WORK, ])
+def get_helper_information(ini=ConfigHelperRainmeter(), **kwargs):
+    log.info("Showing available keyword arguments: {0}".format(str(kwargs.keys())))
     # region Assistant Chat Setup
     assistant_chat = BaseAssistant()
-    assistant_chat.load(assistant_id=assistant_chat.config.app_data['default_assistant_id_desktop'])
+    assistant_chat.load(assistant_id=assistant_chat.config.app_data['assistant_id_desktop'])
 
     def ask_check_desktop():
         messages = [
@@ -85,7 +89,7 @@ def get_helper_information(ini=ConfigHelperRainmeter()):
     # endregion
 
     # region Set dimensions
-    width_multiplier = 2.32
+    width_multiplier = 2.25
     ini['MeterDisplay']['W'] = '({0}*186*#Scale#)'.format(width_multiplier)
     ini['MeterDisplay']['H'] = '((42*#Scale#)+(#ItemLines#*22)*#Scale#)'
     ini['MeterDisplay']['X'] = '14'
@@ -110,7 +114,102 @@ def get_helper_information(ini=ConfigHelperRainmeter()):
 
     # endregion
 
-    ini['Variables']['ItemLines'] = '{0}'.format(len(re.findall(r'\r\n|\r|\n', dump))/3)
+    ini['Variables']['ItemLines'] = '{0}'.format(8)
+
+    return dump
+
+
+_sections__check_world_checks = {
+    "meterLink_github": {
+        "Preset": "InjectedByTest",
+    },
+}
+
+@SPROUT.task()
+@init_meter(RAINMETER_CONFIG,
+            hud_item_name='GPT INFO',
+            new_sections_dict=_sections__check_world_checks,
+            play_sound=False,
+            schedule_categories=[ScheduleCategory.WORK, ])
+def get_events_world_check(countries_list=None, utc_tz="UTC+8", ini=ConfigHelperRainmeter(), **kwargs):
+    log.info("Showing available keyword arguments: {0}".format(str(kwargs.keys())))
+
+    if countries_list is None:
+        return "No countries specified.\n\n\n"
+
+    # region Assistant Chat Setup
+    assistant_chat = BaseAssistant()
+    assistant_chat.load(assistant_id=assistant_chat.config.app_data['assistant_id_reporter'])
+
+    def ask_check_events():
+        messages = [
+            MessageCreate(role='user',
+                          content='Given the list of countries: {0}'
+                                  'Can you get the following information:'
+                                  '- Display their current time if my timezone is {1}'
+                                  '- Weather today from each country'
+                                  '- Notable events that happened today or this week'
+                                  'Make your reply in a plain text paragraphs no bullet points or numbers and do not use markdown.'
+                                  'Be accurate and relevant.'.format(", ".join(countries_list), utc_tz)),
+        ]
+        assistant_chat.add_messages_to_thread(messages)
+        trigger = RunCreate(assistant_id=assistant_chat.properties.id)
+        assistant_chat.run_thread(run=trigger)
+        assistant_chat.wait_for_runs_to_complete()
+        replies = assistant_chat.get_replies()
+        answer = [x.content[0].text.value for x in replies]
+        answer.sort(reverse=True)
+
+        return answer
+
+    # endregion
+
+    # region Set links
+    chat_url = 'https://chatgpt.com/'
+    ini['meterLink']['text'] = "CheatGPT"
+    ini['meterLink']['leftmouseupaction'] = '!Execute ["{0}" 3]'.format(chat_url)
+    ini['meterLink']['tooltiptext'] = chat_url
+    ini['meterLink']['W'] = '100'
+
+    github_work_url = 'https://github.com/brianbartilet/harqis-work'
+    ini['meterLink_github']['Meter'] = 'String'
+    ini['meterLink_github']['MeterStyle'] = 'sItemLink'
+    ini['meterLink_github']['X'] = '(60*#Scale#)'
+    ini['meterLink_github']['Y'] = '(38*#Scale#)'
+    ini['meterLink_github']['W'] = '80'
+    ini['meterLink_github']['H'] = '55'
+    ini['meterLink_github']['Text'] = '|GitHub'
+    ini['meterLink_github']['LeftMouseUpAction'] = '!Execute["{0}" 3]'.format(github_work_url)
+    ini['meterLink_github']['tooltiptext'] = github_work_url
+    # endregion
+
+    # region Set dimensions
+    width_multiplier = 2.25
+    ini['MeterDisplay']['W'] = '({0}*186*#Scale#)'.format(width_multiplier)
+    ini['MeterDisplay']['H'] = '((42*#Scale#)+(#ItemLines#*22)*#Scale#)'
+    ini['MeterDisplay']['X'] = '14'
+    ini['MeterDisplay']['MeasureName'] = 'MeasureLuaScriptScroll'
+
+    ini['MeterBackground']['Shape'] = ('Rectangle 0,0,({0}*190),(36+(#ItemLines#*22)),2 | Fill Color #fillColor# '
+                                       '| StrokeWidth (1*#Scale#) | Stroke Color [#darkColor] '
+                                       '| Scale #Scale#,#Scale#,0,0').format(width_multiplier)
+
+    ini['MeterBackgroundTop']['Shape'] = ('Rectangle 3,3,({0}*186),25,2 | Fill Color #headerColor# | StrokeWidth 0 '
+                                          '| Stroke Color [#darkColor] | Scale #Scale#,#Scale#,0,0').format(width_multiplier)
+    ini['Rainmeter']['SkinWidth'] = '({0}*198*#Scale#)'.format(width_multiplier)
+    ini['Rainmeter']['SkinHeight'] = '((42*#Scale#)+(#ItemLines#*22)*#Scale#)'
+    ini['meterTitle']['W'] = '({0}*190*#Scale#)'.format(width_multiplier)
+    ini['meterTitle']['X'] = '({0}*198*#Scale#)/2'.format(width_multiplier)
+    # endregion
+
+    # region Dump data
+
+    answer_ = ask_check_events()
+    dump = wrap_text(answer_, width=65, indent="\n")
+
+    # endregion
+
+    ini['Variables']['ItemLines'] = '{0}'.format(8)
 
     return dump
 
