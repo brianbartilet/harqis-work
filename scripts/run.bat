d0d0cf3e27dee0ea609da90732eff0131ec6924e
brianbartilet <brian.bartilet@gmail.com>
2025-11-24 22:34:33 +0800

    (work) run script

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,9 +1,8 @@
 @echo off
 git pull
-.\.venv\Scripts\pip.exe install -r requirements.txt --force-reinstall
+REM .\.venv\Scripts\pip.exe install -r requirements.txt --force-reinstall
 
-cd ..\scripts
-call run_workflow_scheduler
+call .\scripts\run_workflow_scheduler
 
-cd ..\scripts
-call run_workflow_worker
+
+call .\scripts\run_workflow_worker
