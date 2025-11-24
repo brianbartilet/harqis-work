e1d58a4cb8aaa389b0b4ea437a430c8efc6cd5d2
brianbartilet <brian.bartilet@gmail.com>
2025-11-25 01:34:58 +0800

    (work) fix bat

--- scripts/set_env_workflows.bat
+++ scripts/set_env_workflows.bat
@@ -19,11 +19,10 @@ if exist "%ENV_FILE%" (
 @echo off
 REM Get the root path of the git repository
 for /f "delims=" %%i in ('git rev-parse --show-toplevel') do set "path_git_root=%%i"
-set "path_core=%path_git_root%\core"
 set "path_demo=%~dp0"
 
 echo Update PYTHONPATH environment variable
-set "PYTHONPATH=%path_git_root%;%path_core%;%path_demo%;%PYTHONPATH%"
+set "PYTHONPATH=%path_git_root%;%path_demo%;%PYTHONPATH%"
 echo PYTHONPATH updated to %PYTHONPATH%.
 
 echo Set environment variable for workflow configuration
