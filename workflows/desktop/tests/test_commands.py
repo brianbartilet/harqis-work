e99485f0ac4d7b1a08746bb68942016d9f81abc4
brianbartilet <brian.bartilet@gmail.com>
2025-11-27 01:23:59 +0800

    (chore) update tests

--- workflows/desktop/tests/test_commands.py
+++ workflows/desktop/tests/test_commands.py
@@ -1,6 +1,8 @@
+import pytest
 from workflows.desktop.tasks.commands import (git_pull_on_paths, set_desktop_hud_to_back, copy_files_targeted,
                                               run_n8n_sequence)
 from workflows.desktop.tasks.capture import run_capture_logging
+
 
 def test__git_pull_on_paths():
     git_pull_on_paths()
@@ -14,5 +16,6 @@ def test__copy_files_targeted():
 def test__run_n8n_sequence():
     run_n8n_sequence()
 
+@pytest.mark.skip(reason="Manual test only")
 def test__run_capture_logging():
     run_capture_logging("DESKTOP")
