af117d387db7718d4085494fbf47b63c6cf4e958
brianbartilet <brian.bartilet@gmail.com>
2025-11-27 07:57:25 +0800

    (work) temp commit

---
+++ workflows/diary/__init__.py
@@ -0,0 +1,6 @@
+# Add import statements for tasks modules to make them accessible
+import workflows.hud.tasks.hud_forex
+import workflows.hud.tasks.hud_tcg
+import workflows.hud.tasks.hud_gpt
+import workflows.hud.tasks.hud_calendar
+import workflows.hud.tasks.hud_logs
