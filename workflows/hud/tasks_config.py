082a6cd0b8c2679ce1da947db00881710658185a
brianbartilet <brian.bartilet@gmail.com>
2025-11-26 11:40:52 +0800

    (test) GPT: testing

--- workflows/hud/tasks_config.py
+++ workflows/hud/tasks_config.py
@@ -52,7 +52,7 @@ WORKFLOWS_HUD = {
 
     'run-job--get_helper_information': {
         'task': 'workflows.hud.tasks.hud_gpt.get_helper_information',
-        'schedule': crontab(minute='32,34,36,38,40'),
+        'schedule': crontab(minute='5'),
         'args': ["DESKTOP_JOBS"],
         'kwargs': {"calendar_cfg_id": "GOOGLE_APPS"},
     },
