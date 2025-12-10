9957b9544ec51e6826016e476a83cc4275831f30
brianbartilet <brian.bartilet@gmail.com>
2025-12-10 23:21:36 +0800

    (work) break down worker queues

--- run_workflows.py
+++ run_workflows.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 from core.apps.sprout.manage import main as sprout_main
 
+from core.apps.sprout.settings import APP_PACKAGE
+from core.config.env_variables import ENV_WORKFLOW_CONFIG
+from core.config.env_variables import ENV_WORKFLOW_CONCURRENCY
+from core.config.env_variables import ENV_WORKFLOW_QUEUE
+print('====================================================================================')
+print(f'{APP_PACKAGE}')
+print(f'{ENV_WORKFLOW_CONFIG}')
+print(f'{ENV_WORKFLOW_CONCURRENCY}')
+print(f'{ENV_WORKFLOW_QUEUE}')
+print('====================================================================================')
 
 if __name__ == '__main__':
     sprout_main()
