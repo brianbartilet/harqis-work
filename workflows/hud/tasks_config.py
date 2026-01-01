bbbd619f96fc5a550ceff125479852872fb80255
brianbartilet <brian.bartilet@gmail.com>
2026-01-01 18:35:09 +0800

    (work) HUD migrate to use kwargs

--- workflows/hud/tasks_config.py
+++ workflows/hud/tasks_config.py
@@ -50,8 +50,9 @@ WORKFLOWS_HUD = {
     'run-job--show_tcg_orders': {
         'task': 'workflows.hud.tasks.hud_tcg.show_tcg_orders',
         'schedule': crontab(minute='*/30'),
-        'args': ["TCG_MP", "SCRYFALL"],
         'kwargs': {
+            "cfg_id__tcg_mp": "TCG_MP",
+            "cfg_id__scryfall": "SCRYFALL",
             "cfg_id__calendar": "GOOGLE_APPS"
         },
         "options": {"queue": "tcg"},
