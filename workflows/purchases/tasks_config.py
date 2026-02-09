e2230dcb7efa5f54aaecbd8e2611244a7fa00980
brianbartilet <brian.bartilet@gmail.com>
2026-02-09 13:02:55 +0800

    (work) rerun auth flow on refresh error

--- workflows/purchases/tasks_config.py
+++ workflows/purchases/tasks_config.py
@@ -72,7 +72,7 @@ WORKFLOW_PURCHASES = {
 
     'run-job--update_tcg_listings_prices': {
         'task': 'workflows.purchases.tasks.tcg_mp_selling.update_tcg_listings_prices',
-        'schedule': crontab(minute=0, hour="2,12,8"),
+        'schedule': crontab(minute=0, hour="2,12"),
         'kwargs': {
             "cfg_id__tcg_mp": "TCG_MP",
             "cfg_id__echo_mtg": "ECHO_MTG",
