b2e236a5d5f435a31fb14f1a468a60ab47e1f5c8
brianbartilet <brian.bartilet@gmail.com>
2026-02-10 21:06:55 +0800

    (work) tcg updates

--- workflows/purchases/tests/test_tcg_mp_selling.py
+++ workflows/purchases/tests/test_tcg_mp_selling.py
@@ -6,7 +6,8 @@ def test__generate_tcg_mappings():
     generate_tcg_mappings(cfg_id__tcg_mp="TCG_MP",
                           cfg_id__echo_mtg="ECHO_MTG",
                           cfg_id__echo_mtg_fe="ECHO_MTG_FE",
-                          cfg_id__scryfall="SCRYFALL"
+                          cfg_id__scryfall="SCRYFALL",
+                          limit=100
                           )
 
 def test__generate_tcg_mappings_force():
@@ -28,7 +29,8 @@ def test__generate_tcg_mappings_bulk():
 def test__generate_tcg_listings():
     generate_tcg_listings(cfg_id__tcg_mp="TCG_MP",
                           cfg_id__echo_mtg="ECHO_MTG",
-                          cfg_id__echo_mtg_fe="ECHO_MTG_FE"
+                          cfg_id__echo_mtg_fe="ECHO_MTG_FE",
+                          limit=100
                           )
 
 def test__generate_tcg_listings_bulk():
