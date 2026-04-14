f9d3110273b4cc43d5dfcf630642870ee5ad943a
brianbartilet <brian.bartilet@gmail.com>
2026-04-14 20:38:37 +0800

    (work) update hud tests

--- workflows/hud/tests/test_hud_tcg.py
+++ workflows/hud/tests/test_hud_tcg.py
@@ -1,7 +1,11 @@
-from workflows.hud.tasks.hud_tcg import show_tcg_orders
+from workflows.hud.tasks.hud_tcg import show_tcg_orders, show_tcg_orders_no_schedule
 
 
 def test__show_tcg_orders():
     show_tcg_orders(cfg_id__tcg_mp="TCG_MP",
                     cfg_id__scryfall="SCRYFALL",
                     cfg_id__calendar="GOOGLE_APPS")
+
+def test__show_tcg_orders_adhoc():
+    show_tcg_orders_no_schedule(cfg_id__tcg_mp="TCG_MP",
+                    cfg_id__scryfall="SCRYFALL")
