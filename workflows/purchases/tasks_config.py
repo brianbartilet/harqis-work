55a95b955561e22bac74362b2a190a6f426549dd
brianbartilet <brian.bartilet@gmail.com>
2026-06-22 00:08:53 +0800

    chore(workflows/purchases): run update_tcg_listings_prices mon+thu

--- workflows/purchases/tasks_config.py
+++ workflows/purchases/tasks_config.py
@@ -90,7 +90,7 @@ WORKFLOW_PURCHASES = {
     # see README "Known issues" #1/#2).
     'run-job--update_tcg_listings_prices': {
         'task': 'workflows.purchases.tasks.tcg_mp_selling.update_tcg_listings_prices',
-        'schedule': crontab(day_of_week="mon", hour='4', minute=0),
+        'schedule': crontab(day_of_week="mon,thu", hour='4', minute=0),
         'kwargs': {
             "cfg_id__tcg_mp": "TCG_MP",
             "cfg_id__echo_mtg": "ECHO_MTG",
